689873864e31d9352e86f389ad67edb89f7d36ef
unknown <akdl911215@naver.com>
2021-05-10 20:38:15 +0900

    1

--- beta/src/collorItem/component/WithoutRedux1.js
+++ beta/src/collorItem/component/WithoutRedux1.js
@@ -1,16 +1,21 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import '../style/withoutRedux.css';
 
 const WithoutRedux1 = () => {
     const onChange = useCallback(() => {
-        document.querySelector('component_red').style.backgroundColor = 'red';
+        document.querySelector('#component_red').style.backgroundColor = 'red';
     });
+
+    const [inputs, setInputs] = useState({});
+
+    const onSubmit = useCallback(() => {});
+
     return (
         <>
             <body>
                 <div className="containerCollrItem" id="component_red">
                     <h1>red</h1>
-                    <input type="button" value="fire" onclick="onChange" />
+                    <input type="button" value="fire" onclick={() => setInputs((inputs.color = 'red'))} />
                 </div>
             </body>
         </>
